d288138c101f4232ee6aeb4dba601a71580fd92c
root <luismichelpcontacto@gmail.com>
2025-03-31 04:32:28 +0000

    amor de cabaret

--- main.h
+++ main.h
@@ -1,10 +1,11 @@
 #include <stdarg.h>
+
 #ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char c);
 int print_string(char *str, int count);
 int _printf(const char *format, ...);
-int handler(char format_char, va_list args, int count);
+int handle_specifier(char format_char, va_list args, int count);
 
 #endif
