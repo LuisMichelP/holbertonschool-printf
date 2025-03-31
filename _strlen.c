948426cb9ce58de7d113f5a602149dc97410bdb6
root <luismichelpcontacto@gmail.com>
2025-03-31 03:58:08 +0000

    ladyfingers

--- _strlen.c
+++ _strlen.c
@@ -1,21 +1,67 @@
-#include "main.h"
+#include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
+#include "main.h"
 
 /**
- * _strlen_recursion - recursion of a char in a sting
- * @s: my pointer
+ * _printf - Custom printf function
+ * @format: Format string containing the characters and specifiers
  *
- * Return: my function
+ * Return: Number of characters printed (excluding null byte)
  */
-
-int _strlen_recursion(char *s)
+int _printf(const char *format, ...)
 {
-	if (*s == '\0')
-	{
-		return (0);
-	}
-	else
-	{
-		return (1 + _strlen_recursion(s + 1));
-	}
+    va_list args;
+    int i = 0, count = 0;
+    char *str;
+
+    if (format == NULL)
+        return (-1);
+
+    va_start(args, format);
+
+    while (format[i] != '\0')
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            if (format[i] == '\0')
+                return (-1);
+
+            if (format[i] == 'c')
+            {
+                count += _putchar(va_arg(args, int));
+            }
+            else if (format[i] == 's')
+            {
+                str = va_arg(args, char *);
+                if (str == NULL)
+                    str = "(null)";
+                
+                int len = _strlen_recursion(str);
+                for (int j = 0; j < len; j++)
+                {
+                    _putchar(str[j]);
+                    count++;
+                }
+            }
+            else if (format[i] == '%')
+            {
+                count += _putchar('%');
+            }
+            else
+            {
+                count += _putchar('%');
+                count += _putchar(format[i]);
+            }
+        }
+        else
+        {
+            count += _putchar(format[i]);
+        }
+        i++;
+    }
+
+    va_end(args);
+    return (count);
 }
