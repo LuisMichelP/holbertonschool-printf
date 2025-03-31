1e454826ef9c96d1bfa26ce13d29f2a0f276b4b1
root <luismichelpcontacto@gmail.com>
2025-03-31 04:58:28 +0000

    integrer

--- print_int.c
+++ print_int.c
@@ -12,32 +12,32 @@
 
 int print_int(int n)
 {
-	int count = 0;
-	char buffer[10];
-	int i = 0;
+    int count = 0;
+    char buffer[10];
+    int i = 0;
 
-	if (n == 0)
-	{
-		count += _putchar('0');
-		return count;
-	}
+    if (n == 0)
+    {
+        count += _putchar('0');
+        return count;
+    }
 
-	if (n < 0)
-	{
-		count += _putchar('-');
-		n = -n;
-	}
-		
-	while (n > 0)
-	{
-		buffer[i++] = (n % 10) + '0';
-		n /= 10;
-	}
+    if (n < 0)
+    {
+        count += _putchar('-');
+        n = -n;
+    }
 
-	for (--i; i >= 0; i--)
-	{
-		count += _putchar(buffer[i]);
-	}
+    while (n > 0)
+    {
+        buffer[i++] = (n % 10) + '0';
+        n /= 10;
+    }
 
-	return count;
+    for (--i; i >= 0; i--)
+    {
+        count += _putchar(buffer[i]);
+    }
+
+    return count;
 }
