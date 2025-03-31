6d02e65fc4cd09742d2d766c79605e3115407f30
root <luismichelpcontacto@gmail.com>
2025-03-31 04:07:24 +0000

    impro

--- _printf.c
+++ _printf.c
@@ -12,56 +12,56 @@
 
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, count = 0;
-    char *str;
+	va_list args;
+	int i = 0, count = 0;
+	char *str;
 
-    if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format[i] != '\0')
-    {
-        if (format[i] == '%')
-        {
-            i++;
-            if (format[i] == '\0')
-                return (-1);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i] == '\0')
+				return (-1);
+			
+			if (format[i] == 'c')
+			{
+				count += _putchar(va_arg(args, int));
+			}
+			else if (format[i] == 's')
+			{
+				str = va_arg(args, char *);
+				if (str == NULL)
+					str = "(null)";
+				while (*str)
+				{
+					_putchar(*str);
+					str++;
+					count++;
+				}
+			}
+			else if (format[i] == '%')
+			{
+				count += _putchar('%');
+			}
+			else
+			{
+				count += _putchar('%');
+				count += _putchar(format[i]);
+			}
+		}
+		else
+		{
+			count += _putchar(format[i]);
+		}
+		i++;
+	}
 
-            if (format[i] == 'c')
-            {
-                count += _putchar(va_arg(args, int));
-            }
-            else if (format[i] == 's')
-            {
-                str = va_arg(args, char *);
-                if (str == NULL)
-                    str = "(null)";
-                
-                for (int j = 0; j < _strlen_recursion(str); j++)
-                {
-                    _putchar(str[j]);
-                    count++;
-                }
-            }
-            else if (format[i] == '%')
-            {
-                count += _putchar('%');
-            }
-            else
-            {
-                count += _putchar('%');
-                count += _putchar(format[i]);
-            }
-        }
-        else
-        {
-            count += _putchar(format[i]);
-        }
-        i++;
-    }
-
-    va_end(args);
-    return (count);
+	va_end(args);
+	return (count);
 }
